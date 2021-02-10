b5533fbd730d05b6ed8e56a861e43c65e9c90a8b
Johan Félisaz <johan.felisaz@protonmail.com>
2021-02-10 12:12:41 +0100

    update

--- scmc.jl
+++ scmc.jl
@@ -1,3 +1,17 @@
-# using Hamiltonian
+using Random
 
 include("hamiltonian.jl")
+
+"Generate a random state (normalized) of shape (3, Ns, Nx, Ny)"
+function randomstate(Ns, Nx, Ny)
+    vec = randn((3, Ns, Nx, Ny))
+    mapslices(v, dims=1) do u
+        normalize!(u)
+    end
+    vec
+end
+
+"Do a single Monte-Carlo step"
+function mcstep(H, v, T)
+    
+end
