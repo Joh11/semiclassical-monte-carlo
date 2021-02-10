dba68c560685c07a07123cdd79483d4e63bcc84c
Johan Félisaz <johan.felisaz@protonmail.com>
2021-02-10 12:21:37 +0100

    finished randomvec

--- scmc.jl
+++ scmc.jl
@@ -1,10 +1,11 @@
 using Random
+using LinearAlgebra
 
 include("hamiltonian.jl")
 
 "Generate a random state (normalized) of shape (3, Ns, Nx, Ny)"
 function randomstate(Ns, Nx, Ny)
-    vec = randn((3, Ns, Nx, Ny))
+    vec = randn(3, Ns, Nx, Ny)
     mapslices(vec, dims=1) do u
         normalize!(u)
     end
@@ -13,5 +14,6 @@ end
 
 "Do a single Monte-Carlo step"
 function mcstep(H, v, T)
+    # choose a random spin
     
 end
