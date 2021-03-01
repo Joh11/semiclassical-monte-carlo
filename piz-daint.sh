cf648a8df9d70cea0fd5d3e19e808cd81271735d
Johan Félisaz <johan.felisaz@protonmail.com>
2021-03-01 15:37:37 +0100

    added submit script

---
+++ piz-daint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --job-name="scmc"
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --account s1008
+
+module load daint-gpu
+module load JuliaExtensions
+
+export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
+srun julia -O3 --check-bounds=no scmc.jl
