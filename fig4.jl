06869c432ab5ed424e6c7d2b402eb55d1c9017de
Johan Félisaz <johan.felisaz@protonmail.com>
2021-03-19 14:41:34 +0100

    attempt at parallelism

--- fig4.jl
+++ fig4.jl
@@ -1,18 +1,19 @@
 include("scmc.jl")
 using Plots
-using Base.Threads: @threads, nthreads
+using Base.Threads: @spawn, threadid, nthreads
 using LaTeXStrings
 using HDF5: h5write
 
 @show nthreads()
 
-Ls = [1, 20, 50, 100]
+# Ls = [1, 20, 50, 100]
+Ls = [1, 20]
 nL = length(Ls)
 T = 1
 dt = 0.1
 nt = 1000
 thermal = 20
-nsamples = 100
+nsamples = 10# 0
 stride = 15
 
 H = loadhamiltonian("hamiltonians/kagome.dat", [1])
@@ -22,27 +23,34 @@ Spipi = zeros(Complex{Float64}, nt, nL)
 S0pi = zeros(Complex{Float64}, nt, nL)
 Spi0 = zeros(Complex{Float64}, nt, nL)
 
+function runsim(v, i, n)
+    vs = simulate(H, v, dt, nt)
+    Sqt = structuralfactor(H, vs, dt)
+    global S00[:, i] += Sqt[1, 1, :] / Sqt[1, 1, 1]
+    global Spipi[:, i] += Sqt[end, end, :] / Sqt[end, end, 1]
+    global Spi0[:, i] += Sqt[end, 1, :] / Sqt[end, 1, 1]
+    global S0pi[:, i] += Sqt[1, end, :] / Sqt[1, end, 1]
+    println("Done simulating the $n th iL=$i in thread $(threadid())")
+end
+
 # simulation itself
-@threads for i = 1:nL
-    L = Ls[i]
+@sync for i = 1:nL
+    @spawn begin
+        L = Ls[i]
 
-    # thermalization
-    v = randomstate(H.Ns, L)
-    mcstep!(H, v, T, thermal)
-
-    println("thermalization for L = $L finished")
-    
-    for n in 1:nsamples
-        mcstep!(H, v, T, stride)
-        vs = simulate(H, v, dt, nt)
-        Sqt = structuralfactor(H, vs, dt)
+        # thermalization
+        v = randomstate(H.Ns, L)
+        mcstep!(H, v, T, thermal)
+        println("thermalization for L = $L finished")
         
-        S00[:, i] += Sqt[1, 1, :] / Sqt[1, 1, 1]
-        Spipi[:, i] += Sqt[end, end, :] / Sqt[end, end, 1]
-        Spi0[:, i] += Sqt[end, 1, :] / Sqt[end, 1, 1]
-        S0pi[:, i] += Sqt[1, end, :] / Sqt[1, end, 1]
+        for n in 1:nsamples
+            mcstep!(H, v, T, stride)
+            @spawn runsim(v, i, n)
+            
+            println("L = $L: sample $n done (thread $(threadid()))")
+        end
+        println("L = $L finished")
     end
-    println("L = $L finished")
 end
 
 S00 /= nsamples
