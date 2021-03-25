9a78ed00f11a7af30800d0e65754c92ee5ee6b20
Johan Félisaz <johan.felisaz@protonmail.com>
2021-03-25 17:14:21 +0100

    new slurm script

--- piz-daint.sh
+++ piz-daint.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name="scmc"
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=36
+#SBATCH --ntasks=36
 #SBATCH --partition=normal
 #SBATCH --constraint=mc
 #SBATCH --account=mr27
-#SBATCH --cpus-per-task=72
+#SBATCH --cpus-per-task=2
 #SBATCH --hint=nomultithread
 
 module load daint-gpu
@@ -14,4 +14,4 @@ module load Julia
 module load JuliaExtensions
 
 export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
-srun julia -O3 --check-bounds=no fig4.jl
+mpirun -n 36 julia -O3 --check-bounds=no fig4.jl
