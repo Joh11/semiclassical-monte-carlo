3e742bd554b5b8d6ef03275ea5c8aa2b6bb288a8
Johan Félisaz <johan.felisaz@protonmail.com>
2021-04-12 07:53:38 +0200

    fix piz daint

--- piz-daint.sh
+++ piz-daint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --job-name="scmc"
-#SBATCH --time=24:00:00
+#SBATCH --time=08:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --partition=normal
+#SBATCH --partition=cscsci
 #SBATCH --constraint=mc
 #SBATCH --account=mr27
-#SBATCH --cpus-per-task=72
+#SBATCH --cpus-per-task=36
 #SBATCH --hint=nomultithread
 #SBATCH --output=fig4.out
 #SBATCH --error=fig4.err
@@ -15,5 +15,5 @@ module load daint-mc
 module load Julia
 module load JuliaExtensions
 
-export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export JULIA_NUM_THREADS=72
 srun julia -O3 --check-bounds=no fig4.jl
