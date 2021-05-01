e4d6e778a6871cb32e60e7fe31c03e774fca8bdf
Johan Félisaz <johan.felisaz@protonmail.com>
2021-05-01 22:38:40 +0200

    write more

--- scripts/bilayer_notebook.jl
+++ scripts/bilayer_notebook.jl
@@ -20,6 +20,12 @@ begin
 	using HDF5
 end
 
+# ╔═╡ d0ead2c6-25fc-4274-bffc-894378e05c2b
+md"# Replicating the paper \"Dynamical structure factors and excitation modes of the bilayer Heisenberg model\""
+
+# ╔═╡ ae4116a7-5594-4300-be63-69c967d6247c
+md"## Replicating figure 1, top row"
+
 # ╔═╡ 9922da8b-6883-4e48-add2-7da92a9ab1e5
 f = h5open("../bilayer-fig1-02.h5", "r") # load data file
 
@@ -60,6 +66,9 @@ function plot_diagram(S, L, nt)
     ret
 end
 
+# ╔═╡ 7b46eff8-fe7f-46da-afc3-d160b887c308
+md"Use the following to cycle between the possible interlayer coupling strengths: "
+
 # ╔═╡ 95134fbc-dd03-460a-8cca-0f03a8dba318
 @bind g Radio(keys(f))
 
@@ -82,6 +91,15 @@ heatmap(real.(Sqomega[1:30, :]),
 	clims=(0, 1)
 )
 
+# ╔═╡ e9d4e665-f8a0-4c7c-9554-47614f90a4e4
+md"## How did I fix the ugly double bands I was obtaining
+
+Contrary to what I was thinking, the frequencies do not take values in \$[0, 2\pi[\$, but it \$[0, 2\pi / dt[\$ (of course, dimensions...). This means that with a smaller timestep, I can reach greater frequencies (sounds obvious now...). 
+
+Probably due to the symmetry of the correlation, the negative and positive frequencies have the same dynamical structure factor values. And we know that the excitations we should get have bounded energy. So if the frequency range is big enough, the positive and negative frequency bands should be totally disconnected. 
+
+I first tried with \$dt = 0.1\$, which worked, but the frequency range was too large compared to the effective spectrum, so I doubled the timestep to 0.2. "
+
 # ╔═╡ d08064e9-3c98-4ec4-9d57-fa94cb1aff71
 md"## Note about the ±ω convention
 
@@ -99,14 +117,18 @@ Even if the heatmaps seem qualitatively correct, we do not have the same magnitu
 - I'm taking the real value of each component. Perhaps the modulus could do better"
 
 # ╔═╡ Cell order:
+# ╟─d0ead2c6-25fc-4274-bffc-894378e05c2b
+# ╟─ae4116a7-5594-4300-be63-69c967d6247c
 # ╠═b3aad6f6-a9f1-11eb-307e-d1f21cd78c7b
 # ╠═9922da8b-6883-4e48-add2-7da92a9ab1e5
 # ╠═0103eb95-d5cf-4988-a8d0-55154dd1d5aa
 # ╟─39fde87d-17f6-4757-8a9f-2c9b2a9f5d48
+# ╟─7b46eff8-fe7f-46da-afc3-d160b887c308
 # ╠═95134fbc-dd03-460a-8cca-0f03a8dba318
 # ╠═160d9e55-bed0-440b-9b57-5af5aad16416
 # ╠═8ae451f1-4d21-4cc6-9093-e05c563bdc2f
 # ╠═23d58720-25b3-46e4-92d5-7a8c5bc03348
 # ╠═fbebc16e-b58e-4d55-b28c-175591ce6987
+# ╟─e9d4e665-f8a0-4c7c-9554-47614f90a4e4
 # ╟─d08064e9-3c98-4ec4-9d57-fa94cb1aff71
-# ╠═2e287fd1-48df-4664-acb3-7e96c9b87e39
+# ╟─2e287fd1-48df-4664-acb3-7e96c9b87e39
