0f2c4498de0c5ef74afa49b37a96e64f56ba7cbf
Johan Félisaz <johan.felisaz@protonmail.com>
2021-05-06 14:41:33 +0200

    single temperature

--- scripts/skl_factor.jl
+++ scripts/skl_factor.jl
@@ -34,12 +34,11 @@ const p = Dict("comment" => "First one, hope it's not too big",
                "J2" => 1,
                "J3" => 1,
                "L" => 10,
-               "Ts" => logrange(5e-3, 0.1, 10),
-               "thermal_first" => 100_000,
-               "thermal" => 10_000,
+               "T" => 0.01,
+               "thermal" => 100_000,
                "nchains" => 72, # because 72 cores on Piz Daint
-               "nsamples_per_chain" => 420, # ~ 30K total samples
-               "stride" => 500,
+               "nsamples_per_chain" => 420, # 420 => ~ 30K total samples
+               "stride" => 100,
                # time evolution stuff
                "dt" => 0.1,
                "nt" => 100)
@@ -49,7 +48,7 @@ const H = loadhamiltonian("hamiltonians/skl.dat", [p["J1"], p["J2"], p["J3"]])
 # variables often used have an alias
 const L = p["L"]
 const nchains = p["nchains"]
-const Ts = p["Ts"]
+const T = p["T"]
 const nsamples_per_chain = p["nsamples_per_chain"]
 const stride = p["stride"]
 const dt = p["dt"]
@@ -60,9 +59,9 @@ const Ns = 6L^2 # number of sites in total
 # Running the simulation
 # ======================
 
-total_St = zeros(ℂ, L, L, nt, length(Ts))
-total_Somega = zeros(ℂ, L, L, nt, length(Ts))
-total_energy = zeros(length(Ts))
+total_St = zeros(ℂ, L, L, nt)
+total_Somega = zeros(ℂ, L, L, nt)
+total_energy = 0
 
 @threads for n in 1:nchains
     println("Starting for chain $n / $nchains ...")
@@ -74,53 +73,46 @@ total_energy = zeros(length(Ts))
 
     St_temp = zeros(ℂ, L, L, nt) # tmp var
 
-    for i in eachindex(Ts)
-        T = Ts[i]
-        println("Starting T = $T for chain $n / $nchains")
-        # reset variables
-        St .= 0
-        Somega .= 0
-        E = 0
-        # thermalization step
-        if i == 1 # hard the first time
-            mcstep!(H, v, T, p["thermal_first"])
-        else
-            mcstep!(H, v, T, p["thermal"])
-        end
-
-        # sampling step
-        for nsample = 1:nsamples_per_chain
-            println("$nsample / $nsamples_per_chain, T = $T (chain $n / $nchains)")
-            flush(stdout)
-            mcstep!(H, v, T, stride)
-            # save the measurements of interest
-            vs = simulate(H, v, dt, nt)
-            St_temp = structuralfactor(H, vs)
-            St += St_temp
-            Somega += frequencystructuralfactor(St_temp)            
-            E += energy(H, v)
-            # use the last time evolved state to improve sampling
-            v = vs[:, :, :, end]
-        end
-
-        # now that everything is sampled, average
-        St /= nsamples_per_chain
-        Somega /= nsamples_per_chain
-        E /= nsamples_per_chain
-
-        # and append to the total results (for all chains)
-        total_St[:, :, :, i] += St
-        total_Somega[:, :, :, i] += Somega
-        total_energy[i] += E
+    println("Starting T = $T for chain $n / $nchains")
+    # reset variables
+    St .= 0
+    Somega .= 0
+    E = 0
+    # thermalization step
+    mcstep!(H, v, T, p["thermal"])
+    
+    # sampling step
+    for nsample = 1:nsamples_per_chain
+        println("$nsample / $nsamples_per_chain (chain $n / $nchains)")
+        flush(stdout)
+        mcstep!(H, v, T, stride)
+        vs = simulate(H, v, dt, nt)
+        
+        # save the measurements of interest        
+        St_temp = structuralfactor(H, vs)
+        St += St_temp
+        Somega += frequencystructuralfactor(St_temp)            
+        E += energy(H, v)
+        
+        # use the last time evolved state to improve sampling
+        v = vs[:, :, :, end]
     end
+    
+    # now that everything is sampled, average
+    St /= nsamples_per_chain
+    Somega /= nsamples_per_chain
+    E /= nsamples_per_chain
+    
+    # and append to the total results (for all chains)
+    global total_St += St
+    global total_Somega += Somega
+    global total_energy += E
 end
 
 # average over all chains
 total_St /= nchains
 total_Somega /= nchains
 total_energy /= nchains
-
-
 
 # Saving
 # ======
@@ -132,14 +124,9 @@ h5open(output, "w") do f
         attributes(f)[name] = val
     end
 
-    for n in eachindex(Ts)
-        T = Ts[n]
-        group = create_group(f, "$n")
-        group["T"] = T
-        @views group["St"] = total_St[:, :, :, n]
-        @views group["Somega"] = total_Somega[:, :, :, n]
-        group["E"] = total_energy[n]
-    end
+    f["St"] = total_St
+    f["Somega"] = total_Somega
+    f["E"] = total_energy
 end
 
 println("Finished !")
