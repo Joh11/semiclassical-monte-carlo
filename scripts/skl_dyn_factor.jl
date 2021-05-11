dd94e6b6eb6d09a8d125ac2073fc78fe720f8338
Johan Félisaz <johan.felisaz@protonmail.com>
2021-05-11 11:32:38 +0200

    dynamical structure factor

--- scripts/skl_dyn_factor.jl
+++ scripts/skl_dyn_factor.jl
@@ -1,9 +1,9 @@
 #=
 
-A script to compute the structure factor (static and dynamical) for
+A script to compute the dynamic structure factor for
 square kagome lattice
 
-Scan over the temperature for J1=J2=J3
+This is for J1=J2=J3
 
 Each chain is indepentent, and stores the result as a temporary
 average. Afterwards, an average is made over all the chains (OK since
@@ -33,13 +33,16 @@ const p = Dict("comment" => "Trying with the extended BZ",
                "J1" => 1,
                "J2" => 1,
                "J3" => 1,
-               "L" => 20,
+               "L" => 10,
                "T" => 0.01,
                "thermal" => 100_000,
                "nchains" => 8, # because 8 cores on my laptop
                "nsamples_per_chain" => 4_000, # so ~ 30k samples
-               "stride" => 100)
-output = "skl_factor_fast.h5"
+               "stride" => 100,
+               # time evolution params
+               "dt" => 1,
+               "nt" => 100)
+output = "skl_dyn_factor.h5"
 const H = loadhamiltonian("hamiltonians/skl.dat", [p["J1"], p["J2"], p["J3"]])
 
 # variables often used have an alias
@@ -48,20 +51,21 @@ const nchains = p["nchains"]
 const T = p["T"]
 const nsamples_per_chain = p["nsamples_per_chain"]
 const stride = p["stride"]
+const nt = p["nt"]
 
 const Nsites = H.Ns * L^2 # number of sites in total
 
 # Running the simulation
 # ======================
 
-total_corr = zeros(H.Ns, L, L, H.Ns, L, L)
+total_corrs = zeros(H.Ns, L, L, H.Ns, L, L, nt)
 total_energy = 0
 
 @threads for n in 1:nchains
     println("Starting for chain $n / $nchains ...")
     
     v = randomstate(H.Ns, L)
-    corr = zeros(H.Ns, L, L, H.Ns, L, L)
+    corrs = zeros(H.Ns, L, L, H.Ns, L, L, nt)
     E = 0
 
     println("Starting T = $T for chain $n / $nchains")
@@ -70,26 +74,37 @@ total_energy = 0
     
     # sampling step
     for nsample = 1:nsamples_per_chain
-        println("$nsample / $nsamples_per_chain (chain $n / $nchains)")
-        flush(stdout)
-        mcstep!(H, v, T, stride)
+        time = @elapsed begin
+            mcstep!(H, v, T, stride)
+
+            # time evolution
+            vs = simulate(H, v, p["dt"], nt)
+            
+            # save the measurements of interest
+            for t = 1:nt
+                @views corrs[:, :, :, :, :, :, t] .+= allcorrelations(vs[:, :, :, t])
+            end
+            E += energy(H, v)
+
+            # use the last time evolved state
+            v = vs[:, :, :, end]
+        end
         
-        # save the measurements of interest
-        corr += allcorrelations(v)
-        E += energy(H, v)
+        println("done $nsample / $nsamples_per_chain (chain $n / $nchains) in $time seconds")
+        flush(stdout)
     end
     
     # now that everything is sampled, average
-    corr /= nsamples_per_chain
+    corrs /= nsamples_per_chain
     E /= nsamples_per_chain
     
     # and append to the total results (for all chains)
-    global total_corr += corr
+    global total_corrs .+= corr
     global total_energy += E
 end
 
 # average over all chains
-total_corr /= nchains
+total_corrs /= nchains
 total_energy /= nchains
 
 # Saving
@@ -102,7 +117,7 @@ h5open(output, "w") do f
         attributes(f)[name] = val
     end
 
-    f["corr"] = total_corr
+    f["corrs"] = total_corrs
     f["E"] = total_energy
 end
 
---
+++ scripts/skl_static_factor.jl
@@ -0,0 +1,109 @@
+#=
+
+A script to compute the static structure factor for
+square kagome lattice
+
+This is for J1=J2=J3
+
+Each chain is indepentent, and stores the result as a temporary
+average. Afterwards, an average is made over all the chains (OK since
+they each have the same number of samples). 
+
+=#
+
+using LinearAlgebra
+using Base.Threads
+using HDF5
+using SemiClassicalMonteCarlo
+const SCMC = SemiClassicalMonteCarlo
+
+const ℂ = Complex{Float64}
+
+# Utility functions
+# =================
+
+function logrange(x1, x2, n)
+    [10^y for y in range(log10(x1), log10(x2), length=n)]
+end
+
+# Params
+# ======
+
+const p = Dict("comment" => "Trying with the extended BZ",
+               "J1" => 1,
+               "J2" => 1,
+               "J3" => 1,
+               "L" => 20,
+               "T" => 0.01,
+               "thermal" => 100_000,
+               "nchains" => 8, # because 8 cores on my laptop
+               "nsamples_per_chain" => 4_000, # so ~ 30k samples
+               "stride" => 100)
+output = "skl_factor_fast.h5"
+const H = loadhamiltonian("hamiltonians/skl.dat", [p["J1"], p["J2"], p["J3"]])
+
+# variables often used have an alias
+const L = p["L"]
+const nchains = p["nchains"]
+const T = p["T"]
+const nsamples_per_chain = p["nsamples_per_chain"]
+const stride = p["stride"]
+
+const Nsites = H.Ns * L^2 # number of sites in total
+
+# Running the simulation
+# ======================
+
+total_corr = zeros(H.Ns, L, L, H.Ns, L, L)
+total_energy = 0
+
+@threads for n in 1:nchains
+    println("Starting for chain $n / $nchains ...")
+    
+    v = randomstate(H.Ns, L)
+    corr = zeros(H.Ns, L, L, H.Ns, L, L)
+    E = 0
+
+    println("Starting T = $T for chain $n / $nchains")
+    # thermalization step
+    mcstep!(H, v, T, p["thermal"])
+    
+    # sampling step
+    for nsample = 1:nsamples_per_chain
+        println("$nsample / $nsamples_per_chain (chain $n / $nchains)")
+        flush(stdout)
+        mcstep!(H, v, T, stride)
+        
+        # save the measurements of interest
+        corr += allcorrelations(v)
+        E += energy(H, v)
+    end
+    
+    # now that everything is sampled, average
+    corr /= nsamples_per_chain
+    E /= nsamples_per_chain
+    
+    # and append to the total results (for all chains)
+    global total_corr += corr
+    global total_energy += E
+end
+
+# average over all chains
+total_corr /= nchains
+total_energy /= nchains
+
+# Saving
+# ======
+
+println("Saving to $output ...")
+h5open(output, "w") do f
+    # save params
+    for (name, val) in p
+        attributes(f)[name] = val
+    end
+
+    f["corr"] = total_corr
+    f["E"] = total_energy
+end
+
+println("Finished !")
