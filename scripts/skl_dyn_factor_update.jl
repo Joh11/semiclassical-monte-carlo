63c87c9d07bc7489edce7b447f774810749de236
Johan Félisaz <johan.felisaz@protonmail.com>
2021-06-09 09:23:43 +0200

    update

---
+++ scripts/skl_dyn_factor_update.jl
@@ -0,0 +1,69 @@
+#=
+A script to update only the structure factor from a HDF5 dataset
+=#
+
+using HDF5
+using SemiClassicalMonteCarlo
+const SCMC = SemiClassicalMonteCarlo
+
+const ℂ = Complex{Float64}
+
+oldfilename = "skl_dyn_factor.h5"
+newfilename = "skl_dyn_factor_fixed.h5"
+
+const p = Dict("comment" => "Trying with the extended BZ",
+               "J1" => 1,
+               "J2" => 1,
+               "J3" => 1,
+               "L" => 10,
+               "T" => 0.01,
+               "thermal" => 1,# 00_000,
+               "nchains" => 8, # because 8 cores on my laptop
+               "nsamples_per_chain" => 4_000,# 4_000, # so ~ 30k samples
+               "stride" => 100,
+               # time evolution params
+               "dt" => 1,
+               "nt" => 100)
+
+const nt = p["nt"]
+
+# read from old
+corr = nothing
+E = nothing
+
+h5open(oldfilename, "r") do f
+    corr = f["corr"]
+    E = f["E"]
+end
+
+# now compute the structure factor
+println("Now computing structure factor ...")
+kxs = 8π * (-1:0.05:1)
+kys = 8π * (-1:0.05:1)
+Sqt = zeros(ℂ, length(kxs), length(kys), nt)
+for t = 1:nt
+    println("Doing $t / $nt ...")
+    @views Sqt[:, :, t] = structurefactors(H, corr[:, :, :, :, :, :, t], kxs, kys)
+end
+
+# now compute the frequency resolved structure factor
+println("Now computing (frequency resolved) structure factor ...")
+Sqω = frequencystructuralfactor(Sqt)
+
+# Saving
+# ======
+
+println("Saving to $newfilename ...")
+h5open(newfilename, "w") do f
+    # save params
+    for (name, val) in p
+        attributes(f)[name] = val
+    end
+
+    f["corr"] = corr
+    f["Sqt"] = Sqt
+    f["Sqω"] = Sqω
+    f["E"] = E
+end
+
+println("Finished !")
