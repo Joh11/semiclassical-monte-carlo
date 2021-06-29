2a390336d5b2c35f1685679a1103bbc62b5b8d54
Johan Félisaz <johan.felisaz@protonmail.com>
2021-06-29 14:44:59 +0200

    refactor for clarity

--- scripts/skl_dyn_factor_update.jl
+++ scripts/skl_dyn_factor_update.jl
@@ -1,67 +1,44 @@
 #=
-A script to update only the structure factor from a HDF5 dataset
+A script to add the whole (extended) BZ structure factor S(q) to a HDF5 dataset
 =#
 
 using HDF5
 using SemiClassicalMonteCarlo
 const SCMC = SemiClassicalMonteCarlo
 
-const ℂ = Complex{Float64}
+# Functions
+# =========
 
-oldfilename = "data/skl_dyn_factor.h5"
-newfilename = "data/skl_dyn_factor_uc.h5"
+function includeBZstructurefactor(f, H)
+    # read corr (at zero time) from old (+ all other necessary variables)
+    @views corr0 = read(f["corr"])[:, :, :, :, :, :, 1]
 
-const p = Dict("comment" => "In the non extended UC",
-               "J1" => 1,
-               "J2" => 1,
-               "J3" => 1,
-               "L" => 10,
-               "T" => 0.01,
-               "thermal" => 1,# 00_000,
-               "nchains" => 8, # because 8 cores on my laptop
-               "nsamples_per_chain" => 4_000,# 4_000, # so ~ 30k samples
-               "stride" => 100,
-               # time evolution params
-               "dt" => 1,
-               "nt" => 100)
+    kxs = 4π * Array(-1:0.01:1)
+    kys = 4π * Array(-1:0.01:1)
+    
+    St0 = structurefactors(H, corr0, kxs, kys)
 
-const H = loadhamiltonian("hamiltonians/skl.dat", [p["J1"], p["J2"], p["J3"]])
-const nt = p["nt"]
-
-# read from old
-f = h5open(oldfilename, "r")
-corr = read(f["corr"])
-E = read(f["E"])
-close(f)
-
-# now compute the structure factor
-println("Now computing structure factor ...")
-kxs = π * Array(-1:0.01:1)
-kys = π * Array(-1:0.01:1)
-Sqt = zeros(ℂ, length(kxs), length(kys), nt)
-for t = 1:nt
-    println("Doing $t / $nt ...")
-    @views Sqt[:, :, t] = structurefactors(H, corr[:, :, :, :, :, :, t], kxs, kys)
-end
-
-# now compute the frequency resolved structure factor
-println("Now computing (frequency resolved) structure factor ...")
-Sqω = frequencystructuralfactor(Sqt)
-
-# Saving
-# ======
-
-println("Saving to $newfilename ...")
-h5open(newfilename, "w") do f
-    # save params
-    for (name, val) in p
-        attributes(f)[name] = val
+    # save
+    if haskey(f, "St0")
+        println("Deleting old content ...")
+        delete_object(f, "St0")
     end
-
-    f["corr"] = corr
-    f["Sqt"] = Sqt
-    f["Sqω"] = Sqω
-    f["E"] = E
+    
+    write(f, "St0", real.(St0))
 end
+
+# Script itself
+# =============
+
+filename = ARGS[1]
+f = h5open(filename, "r+")
+
+J1 = read(attributes(f)["J1"])
+J2 = read(attributes(f)["J2"])
+J3 = read(attributes(f)["J3"])
+
+const H = loadhamiltonian("hamiltonians/skl.dat", [J1, J2, J3])
+
+includeBZstructurefactor(f, H)
 
 println("Finished !")
